39372ea35e8c79395b045061d7d162f8b79ec5cf
chaithragp12 <83128569+chaithragp12@users.noreply.github.com>
2024-10-30 21:16:03 +0530

    Update RollDice.jsx

--- src/components/RollDice.jsx
+++ src/components/RollDice.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import image1 from "../assets/images/image1.png"
+import image2 from "../assets/images/image2.png"
+import image3 from "../assets/images/image3.png"
+import image4 from "../assets/images/image4.png"
+import image5 from "../assets/images/image5.png"
+import image6 from "../assets/images/image6.png"
 const RollDice = ({currentDate, roleDice}) => {
 
  
@@ -8,7 +13,7 @@ const RollDice = ({currentDate, roleDice}) => {
   return (
     <DiceContainer>
       <div className='dice' onClick={roleDice}> 
-        <img src={image1} alt="dice Image"
+       <img src={currentDate==1 ? image1 :currentDate==2 ? image2 :currentDate==3 ? image3 :currentDate==4 ? image4 :currentDate==5 ? image5 :image6} alt="dice game"
       /></div>
       <p>Click On Dice to Roll</p>
      
